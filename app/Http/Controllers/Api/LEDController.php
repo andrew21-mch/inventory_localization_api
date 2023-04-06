07f9444f0ded5d8f9017864a8378886326edb431
andrew21-mch <nfonandrew73@gmail.com>
2023-04-06 15:48:52 +0100

    led draft

---
+++ app/Http/Controllers/Api/LEDController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LEDController extends Controller
+{
+
+    // make api request to trigger led
+    public function triggerLED(Request $request)
+    {
+        $url = 'http://localhost:3000/led/trigger';
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', $url, [
+            'form_params' => [
+                'led' => $request->led_number,
+                'status' => $request->status
+            ]
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'led triggered successfully',
+            'data' => json_decode($response->getBody()->getContents())
+        ]);
+
+    }
+
+    // make api request to get led status
+    public function getLEDStatus(Request $request)
+    {
+        $url = 'http://localhost:3000/led/status';
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', $url, [
+            'form_params' => [
+                'led' => $request->led_number
+            ]
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'led status fetched successfully',
+            'data' => json_decode($response->getBody()->getContents())
+        ]);
+    }
+
+    // install led
+    public function installLED(Request $request)
+    {
+        $url = 'http://localhost:3000/led/install';
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', $url, [
+            'form_params' => [
+                'led' => $request->led_number
+            ]
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'led installed successfully',
+            'data' => json_decode($response->getBody()->getContents())
+        ]);
+    }
+
+    
+}
