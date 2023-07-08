8eb54bf7b5f4573b8dc31bc3f3262ee378c7ea0f
andrew21-mch <nfonandrew73@gmail.com>
2023-07-08 08:06:47 +0100

    update: server image upload

--- app/Http/Controllers/Api/ComponentController.php
+++ app/Http/Controllers/Api/ComponentController.php
@@ -8,6 +8,7 @@ use App\Models\Component;
 use App\Models\OutOfStock;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class ComponentController extends Controller
@@ -42,15 +43,25 @@ class ComponentController extends Controller
         }
 
         \DB::beginTransaction();
-        try{
-            if($request->supplier_id){
+        try {
+            if ($request->supplier_id) {
                 $supplier = Supplier::find($request->supplier_id);
-                if(!$supplier){
+                if (!$supplier) {
                     return ApiResponse::errorResponse('supplier not found', null, 404);
                 }
             }
 
-            if(!$request->supplier_id && $request->add_supplier){
+            $imagePath = null;
+
+            if ($request->has('image')) {
+                $imageData = $request->input('image');
+                $image = base64_decode($imageData);
+                $imagePath = 'items/' . uniqid() . '.jpg'; // Generate a unique filename
+                Storage::disk('public')->put($imagePath, $image);
+            }
+
+
+            if (!$request->supplier_id && $request->add_supplier) {
                 $supplier = Supplier::create([
                     'name' => $request->supplier_name,
                     'phone' => $request->supplier_phone,
@@ -64,7 +75,7 @@ class ComponentController extends Controller
                 'quantity' => $request->quantity,
                 'price_per_unit' => $request->price_per_unit,
                 'cost_price_per_unit' => $request->cost_price_per_unit,
-                'image' => $request->image_uri,
+                'image' => $imagePath,
                 'slug' => self::createSlug($request->name),
                 'description' => $request->description,
                 'supplier_id' => $request->supplier_id ?? $supplier->id,
@@ -73,37 +84,35 @@ class ComponentController extends Controller
 
 
             $ledTriggered = false;
-            if($request->led_id){
+            if ($request->led_id) {
                 $ledTriggered = LEDController::testLed($request->led_id);
             }
 
-            if($component->quantity <= 10){
-                try
-                {
+            if ($component->quantity <= 10) {
+                try {
                     $outOfStock = OutOfStock::where('component_id', $component->id)->first();
-                    if($outOfStock){
+                    if ($outOfStock) {
                         $outOfStock->update([
                             'component_id' => $component->id,
                             'supplier_id' => $component->supplier_id,
                         ]);
-                    }else{
+                    } else {
                         OutOfStockController::store($component->id, $component->supplier_id);
                     }
-                }
-                catch(\Exception $e)
-                {
+                } catch (\Exception $e) {
                     return ApiResponse::errorResponse('something went wrong', $e->getMessage(), 500);
                 }
             }
 
             \DB::commit();
-            if($ledTriggered){
+            if ($ledTriggered) {
                 return ApiResponse::successResponse('component created successfully', $component, 201);
-            }else{
+            } else {
                 return ApiResponse::successResponse('component created successfully, but led not triggered', $component, 201);
             }
 
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
+            \DB::rollback();
             return ApiResponse::errorResponse('something went wrong', $e->getMessage(), 500);
         }
     }
@@ -114,7 +123,7 @@ class ComponentController extends Controller
     public function show($id)
     {
         $component = Component::find($id);
-        if(!$component){
+        if (!$component) {
             return ApiResponse::errorResponse('component not found', null, 404);
         }
 
@@ -127,25 +136,25 @@ class ComponentController extends Controller
     public function update(Request $request, string $id)
     {
         $component = Component::find($id);
-        if(!$component){
+        if (!$component) {
 
             return ApiResponse::errorResponse('component not found', null, 404);
         }
 
         $name = $component->image;
-        if($request->hasFile('image')){
+        if ($request->hasFile('image')) {
             $image = $request->file('image');
-            $name = time().'.'.$image->getClientOriginalExtension();
+            $name = time() . '.' . $image->getClientOriginalExtension();
             $destinationPath = public_path('/images');
             $image->move($destinationPath, $name);
-            $oldImage = public_path('/images/').$component->image;
-            if(file_exists($oldImage)){
+            $oldImage = public_path('/images/') . $component->image;
+            if (file_exists($oldImage)) {
                 @unlink($oldImage);
             }
         }
 
         \DB::beginTransaction();
-        try{
+        try {
 
             // update using request->all() will update the slug
             $component->update([
@@ -160,7 +169,7 @@ class ComponentController extends Controller
             ]);
             \DB::commit();
             return ApiResponse::successResponse('component updated successfully', $component, 200);
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             \DB::rollBack();
             return ApiResponse::errorResponse('something went wrong', $e->getMessage(), 500);
         }
@@ -172,21 +181,21 @@ class ComponentController extends Controller
     public function destroy($id)
     {
         $component = Component::with('led', 'sales', 'outOfStocks')->where('id', $id)->first();
-        if(!$component){
+        if (!$component) {
             ApiResponse::errorResponse('component not found', null, 404);
         }
 
         \DB::beginTransaction();
-        try{
+        try {
 
-            if($component->sales->count() > 0 || $component->outOfStocks->count() > 0){
+            if ($component->sales->count() > 0 || $component->outOfStocks->count() > 0) {
                 return ApiResponse::errorResponse('component cannot be deleted because it has sales or out of stock', null, 422);
             }
 
             $component->delete();
             \DB::commit();
             return ApiResponse::successResponse('component deleted successfully', null, 200);
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             \DB::rollBack();
             return ApiResponse::errorResponse('something went wrong', $e->getMessage(), 500);
         }
@@ -198,33 +207,30 @@ class ComponentController extends Controller
     public function search(Request $request)
     {
         $components = Component::where('name', 'LIKE', "%{$request->search}%")
-        ->orWhere('description', 'LIKE', "%{$request->search}%")
-        ->orWhere('quantity', 'LIKE', "%{$request->search}%")
-        ->orWhere('identifier', 'LIKE', "%{$request->search}%")
-        ->orWhere('price_per_unit', 'LIKE', "%{$request->search}%")
-        ->orWhere('cost_price_per_unit', 'LIKE', "%{$request->search}%")
-        ->orWhereHas('supplier', function($query) use ($request){
-            $query->where('name', 'LIKE', "%{$request->search}%")
-            ->orWhere('phone', 'LIKE', "%{$request->search}%");
-        })->with('led')->get();
+            ->orWhere('description', 'LIKE', "%{$request->search}%")
+            ->orWhere('quantity', 'LIKE', "%{$request->search}%")
+            ->orWhere('identifier', 'LIKE', "%{$request->search}%")
+            ->orWhere('price_per_unit', 'LIKE', "%{$request->search}%")
+            ->orWhere('cost_price_per_unit', 'LIKE', "%{$request->search}%")
+            ->orWhereHas('supplier', function ($query) use ($request) {
+                $query->where('name', 'LIKE', "%{$request->search}%")
+                    ->orWhere('phone', 'LIKE', "%{$request->search}%");
+            })->with('led')->get();
 
-        if($components){
-            foreach($components as $component){
-                if($component->quantity <= 10){
-                    try
-                    {
+        if ($components) {
+            foreach ($components as $component) {
+                if ($component->quantity <= 10) {
+                    try {
                         $outOfStock = OutOfStock::where('component_id', $component->id)->first();
-                        if($outOfStock){
+                        if ($outOfStock) {
                             $outOfStock->update([
                                 'component_id' => $component->id,
                                 'supplier_id' => $component->supplier_id,
                             ]);
-                        }else{
+                        } else {
                             OutOfStockController::store($component->id, $component->supplier_id);
                         }
-                    }
-                    catch(\Exception $e)
-                    {
+                    } catch (\Exception $e) {
                         return ApiResponse::errorResponse('something went wrong', $e->getMessage(), 500);
                     }
                 }
@@ -247,7 +253,7 @@ class ComponentController extends Controller
     public static function formatComponents($components)
     {
         $formattedComponents = [];
-        foreach($components as $component){
+        foreach ($components as $component) {
             $formattedComponents[] = [
                 'id' => $component->id,
                 'name' => $component->name,
